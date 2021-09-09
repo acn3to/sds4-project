e12271372f26ebcfa09974ab31496ab19282ded7
Costa Neto <costanetoow@gmail.com>
2021-09-09 08:59:16 -0300

    Donut chart title changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             <BarChart />
           </div>
           <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <h5 className="text-center text-secondary">Todas vendas</h5>
             <DonutChart />
           </div>      
         </div>
