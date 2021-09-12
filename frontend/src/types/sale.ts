fac77527d81a2dbe027fc3408a55b2246ddfa290
Costa Neto <costanetoow@gmail.com>
2021-09-12 00:06:24 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum =  {
-    sellername: string,
-    sum: number
+    sellername: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sellername: string;
+    visited: number;
+    deals: number;
 }
