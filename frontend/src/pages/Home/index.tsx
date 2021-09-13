f503b546669d2b7c9e3166ee2aec42f8399e399c
Costa Neto <costanetoow@gmail.com>
2021-09-13 01:14:24 -0300

    DSVendas to DS-Vendas

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <NavBar />
             <div className="container">
                 <div className="jumbotron">
-                    <h1 className="display-4">DSVendas</h1>
+                    <h1 className="display-4">DS-Vendas</h1>
                     <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                     <hr />
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
